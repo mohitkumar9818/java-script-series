1440fc9419c1c7b1c6afcfa2fc66350045340205
mohit kumar <143921336+mohitkumar9818@users.noreply.github.com>
2024-12-28 08:11:35 +0000

    variable and data type

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,25 @@
-console.log("Mohit")
+//console.log("Mohit kumar")
+//------------------------------------------------------------------------------------
+//variables 
+// const accountId="mohit@123"
+// let accountEmail="mohit@gmail.com"
+// var accountPassword="12344"//dont use var in js 
+// number =8826213533
+// console.log(number)
+// let number1;//undefined
+// console.table([accountEmail,accountId,accountPassword,number,number1])
+//--------------------------------------------------------------------------------------
+
+//data types in js 
+// n1=null;
+// Number=10 //2 ki power 53 range
+// BigInt=20
+// String="mohit"
+// null
+// undefined
+// Symbol
+// Boolean =true;
+// console.log([Number,BigInt,String,null,Boolean])
+// console.log(typeof null,undefined)//object,undefined 
+//-----------------------------------------------------------------------
+
