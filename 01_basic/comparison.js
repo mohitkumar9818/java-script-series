81198cee9b7b95479bb87cbd2a464dcf4f420cd7
mohit kumar <143921336+mohitkumar9818@users.noreply.github.com>
2024-12-28 18:14:18 +0000

    comparison of data types

---
+++ 01_basic/comparison.js
@@ -0,0 +1,31 @@
+// console.log(2>1)//true
+// console.log(2>=1)//true
+// console.log(2==1)//false
+// console.log(2<1)//false
+// console.log(2!=1)//true
+// console.log(2<=1)//false
+//------------------------------------------------
+// console.log("2">1)//true yha string 2 ko number 2 int consider kiya gya hai js me 
+// console.log("02">1)//true
+ console.log("2"==2)//t  yha pr string ko number me convert kr deta hai js 
+// console.log("02">4)//f
+//--------------------------------------------------
+
+// console.log(null >0)//f
+// console.log(null ==0)//f
+// console.log(null >=0)//t
+// console.log(null >1)//f
+console.log(null <1)//t
+// console.log(null ==1)//f
+// console.log(null >=1)//f
+//----------------------------------------------
+// console.log(undefined >0)//f
+// console.log(undefined ==0)//f
+// console.log(undefined >=0)//f
+
+// console.log(undefined >1)//f
+// console.log(undefined ==1)//f
+// console.log(undefined >=1)//f
+console.log(undefined <1)//f
+
+//-----------------------------------------------------
