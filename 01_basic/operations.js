718267d66a8ab1aea57ccdfae1dd9da25e8b33c2
mohit kumar <143921336+mohitkumar9818@users.noreply.github.com>
2024-12-28 17:48:23 +0000

    basic operation ,conversion,prefix,postfix

---
+++ 01_basic/operations.js
@@ -0,0 +1,22 @@
+// console.log(10+2)
+// console.log(1-2)
+// console.log(1*2)
+// console.log(1/2)
+// console.log(1**2)
+// console.log(1%2)
+//-----------------------------------------------------------------------------------------
+//conversion
+// console.log(1+"2")
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+//if string first then convertion in string if other thing is first then it will convert first 
+//---------------------------------------------------------------------------------------------
+let gamecounter=100
+
+++gamecounter;//101
+console.log(gamecounter)//out: 101 --prefix 
+gamecounter++;//101 (+1 next time call gamecounter then it will automatically change its value)
+console.log(gamecounter)//postfix out: 102
+console.log(gamecounter++)//out:102
+console.log(gamecounter)//out:103
