91be109d1a59d89f2ce8ddc59dbed263a7b81a4d
mohit kumar <143921336+mohitkumar9818@users.noreply.github.com>
2024-12-28 10:58:51 +0000

    conversion of datatypes

---
+++ 01_basic/conversion.js
@@ -0,0 +1,15 @@
+// let score = "33abc"
+// let score1=true
+// console.log(typeof score,score1)
+// let valueInnumber=Number(score1)
+// console.log(valueInnumber)//NaN
+// let login=1
+// console.log(Boolean(login))
+//33=>33
+//"33abc"=>NaN
+//true=>1 , false=0
+//
+let names="mohit"
+let name1=""
+console.log(Boolean(names))//true
+console.log(Boolean(name1))//false
