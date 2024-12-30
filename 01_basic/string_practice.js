566e169e884ed509368942bcc0e47fa317a54988
mohit kumar <143921336+mohitkumar9818@users.noreply.github.com>
2024-12-30 09:48:57 +0000

    str

--- 01_basic/string_practice.js
+++ 01_basic/string_practice.js
@@ -1,13 +1,12 @@
 const greeting = '   Hello world!   ';
-
 console.log(greeting);
 // Expected output: "   Hello world!   ";
 
 console.log(greeting.trimEnd());
 // Expected output: "   Hello world!";
-
 const greeting1 = '   Hello world!   ';
 console.log(greeting1);
+
 // Expected output: "   Hello world!   ";
 console.log(greeting1.trimStart());
 // Expected output: "Hello world!   ";
