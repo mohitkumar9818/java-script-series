779cd8525048b77a6b242690f73c9f1bd5366352
mohit kumar <143921336+mohitkumar9818@users.noreply.github.com>
2025-01-01 14:20:03 +0000

    operationa

--- 01_basic/operations.js
+++ 01_basic/operations.js
@@ -13,7 +13,6 @@
 //if string first then convertion in string if other thing is first then it will convert first 
 //---------------------------------------------------------------------------------------------
 let gamecounter=100
-
 ++gamecounter;//101
 console.log(gamecounter)//out: 101 --prefix 
 gamecounter++;//101 (+1 next time call gamecounter then it will automatically change its value)
