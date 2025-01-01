7c7197b2a17cfe690a187ceaa6c3c2579d858f9b
mohit kumar <143921336+mohitkumar9818@users.noreply.github.com>
2025-01-01 14:20:51 +0000

    memory

--- 01_basic/memory(stack,heap).js
+++ 01_basic/memory(stack,heap).js
@@ -6,8 +6,6 @@ let anothername=ytname // copy mila hai ytname ka
 anothername="mohitkumar"//anothername ki value chnage ho jayega  
 console.log(anothername)  //out: mohit kumar 
 console.log(ytname)//out:mkbrain //yha value samee rhega
-
-
 //Heap(non_primitive) -- isme hme direct refrence milta hai jo bhi value hai 
 
 let ytchen={yt1:"mk",
