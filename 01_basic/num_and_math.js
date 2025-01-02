289f53caf3e11a65a21341352b37f89ae03714cc
mohit kumar <143921336+mohitkumar9818@users.noreply.github.com>
2025-01-02 17:00:22 +0000

    num and math

---
+++ 01_basic/num_and_math.js
@@ -0,0 +1,7 @@
+const score=400
+const bal =new Number (100)
+console.log(score)//400
+console.log(bal)//[Number: 100]
+console.log(bal.toString().length)//3
+console.log(bal.toFixed(2))//100.00
+console.log(bal.toFixed(4))//100.0000
