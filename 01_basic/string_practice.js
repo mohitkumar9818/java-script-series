c94bb6abe53edfe2586c21d165f4b28dea414b31
mohit kumar <143921336+mohitkumar9818@users.noreply.github.com>
2025-01-02 17:12:46 +0000

    length add

--- 01_basic/string_practice.js
+++ 01_basic/string_practice.js
@@ -10,3 +10,10 @@ console.log(greeting1);
 // Expected output: "   Hello world!   ";
 console.log(greeting1.trimStart());
 // Expected output: "Hello world!   ";
+const name="mohit kumar"
+const name2 = "mohit kumar sinha"
+name3="   mohit    kumar     sinha"
+console.log(name.length)
+console.log(name2.length)
+console.log(name3.trimEnd())
+console.log(name3.length)
