de105c5a4a202dd5cb3b3c62bcca141bd6388e58
mohit kumar <143921336+mohitkumar9818@users.noreply.github.com>
2025-01-03 09:47:29 +0000

    math and num

--- 01_basic/num_and_math.js
+++ 01_basic/num_and_math.js
@@ -1,7 +1,41 @@
-const score=400
-const bal =new Number (100)
-console.log(score)//400
-console.log(bal)//[Number: 100]
-console.log(bal.toString().length)//3
-console.log(bal.toFixed(2))//100.00
-console.log(bal.toFixed(4))//100.0000
+// const score=400
+// const bal =new Number (100)
+// console.log(score)//400
+// console.log(bal)//[Number: 100]
+// console.log(bal.toString().length)//3
+// console.log(bal.toFixed(2))//100.00
+// console.log(bal.toFixed(4))//100.0000
+
+// const num=100.2003
+// console.log(num.toPrecision(3))//100
+// const num2=10.2003
+// console.log(num2.toPrecision(3))//10.2
+// const num3=3862.7803
+// console.log(num3.toPrecision(5))//3862.8
+// const num4=1239.2003
+// console.log(num4.toPrecision(1))//1e+3
+
+// const hundred=100000
+// console.log(hundred.toLocaleString())//100,000
+// const hundred_ind=100000
+// console.log(hundred.toLocaleString("en-IN"))//1,00,000
+//--------------------------------Math-------------------------------------------------------
+// console.log(Math)
+// console.log(Math.abs(-4))//4
+// console.log(Math.floor(4.9))//4
+// console.log(Math.ceil(4.2))//5
+// console.log(Math.round(4.6))//5
+// console.log(Math.round(4.5))//5
+// console.log(Math.round(4.4))//4
+// console.log(Math.sqrt(25))//5
+// console.log(Math.min(2,3,55,3,4))//2
+// console.log(Math.max(2,3,55,3,4))//55
+
+
+// console.log(Math.random())//0.4267648650822111
+// console.log(Math.random()*10)//9.552970026650257
+// console.log(Math.floor(Math.random()*10))//3
+
+const min =10
+const max =20
+console.log(Math.floor(Math.random()*(max-min +1))+min)///15 or 17 or 18 etc 
